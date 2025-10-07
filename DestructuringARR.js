19102e9b9ddc6a7893c2f5bbcbe616d4d2cb3856
Bhupendra Singh <bs4004908@gmail.com>
2025-10-07 11:50:13 +0530

    new file

--- DestructuringARR.js
+++ DestructuringARR.js
@@ -1,10 +1,39 @@
 /// is a powerful feature that allows for unpacking values 
 // from arrays or properties from objects into distinct variables.
 
-const person = { name: 'Alice', age: 30, city: 'New York' };
+// let user = ["Yahoo Baba", 22, "Delhi"];
 
-// Destructuring to extract name and age
-const { name, age } = person;
+// let [name, age , city] = user;   // destructuing
+// console.log(name);
+// console.log(age);
+// console.log(city);
 
-console.log(name); // Alice
-console.log(age);  // 30
+// using rest operator
+
+// let user = ["Yahoo baba" , 20, "Delhi"];
+
+// let [name, ...args] = user;
+// console.log(name);
+// console.log(args);
+
+// uisng function
+
+// function user (name , age = 20 , city){
+//     console.log(name);
+//     console.log(age);
+//     console.log(city);
+// }
+
+// user (["Yahoo baba" , 20, "Delhi"]);
+
+
+// using return array
+
+function user(){
+    return ["Yahoo baba", 22, "agra"];
+}
+let [name, age, city] = user();
+
+console.log(city);
+console.log(age);
+
---
+++ Oops.js
@@ -0,0 +1,20 @@
+class hello {
+    message() {   // method 
+        console.log("hello everyone");
+    }
+    sorry(){
+        console.log("sorry");
+        
+    }
+}
+
+let a = new hello();     // object
+a.message();
+a.sorry();
+
+// types of method 
+// CONSTRUCTOR
+// PROTOTYPE
+// STATIC
+
+
