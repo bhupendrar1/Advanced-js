4a813604ac47d255ea19c26940921e78064d96e2
Bhupendra Singh <bs4004908@gmail.com>
2025-10-11 12:19:10 +0530

    Ajax

--- Ajax.js
+++
@@ -1,4 +0,0 @@
-// AJAX, which stands for Asynchronous JavaScript and XML,
-//  is a set of web development techniques that allows web applications 
-// to send and retrieve data from a server asynchronously without interfering 
-// with the display and behavior of the existing page
